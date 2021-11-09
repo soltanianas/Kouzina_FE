9bac23f9ee50bba1a67fe8d7ecbaa4ea66eac650
soltanianas <soltani.anas@gmail.com>
2021-11-09 09:11:16 +0100

    New Scences +

---
+++ Kouzina/boch/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  boch
+//
+//  Created by MacBook Pro on 4/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    class secondViewController: UIViewController {
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+}
+
